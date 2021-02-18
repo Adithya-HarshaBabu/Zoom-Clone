d3b6fe7785372b7526e812120fb74ee4bc47fa0e
Adithya Harsha Babu <ahb437@nyu.edu>
2021-02-17 21:44:51 -0500

    peer

--- public/script.js
+++ public/script.js
@@ -1,8 +1,15 @@
+const socket = io("/");
+
 let myVideoStream;
 const videoGrid = document.getElementById("video-grid");
 
 const myVideo = document.createElement("video");
 myVideo.muted = true;
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "3030",
+});
 navigator.mediaDevices
   .getUserMedia({
     video: true,
@@ -12,7 +19,21 @@ navigator.mediaDevices
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
   });
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
 
+socket.on("user-connected", (userId) => {
+  connectToNewUser(userId);
+});
+
+const connectToNewUser = (userId) => {
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoToStream(viseo, userVideoStream);
+  });
+};
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
   video.addEventListener("loadedmetadata", () => {
